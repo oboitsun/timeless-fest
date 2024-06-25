382321ded80daba683d5688530ab3e04b236b662
oboitsun <thewip3out@gmail.com>
2024-06-25 12:44:31 +0300

    added new footer updates

--- components/PromoModal.js
+++ components/PromoModal.js
@@ -42,9 +42,9 @@ export default function PromoModal() {
             width="100%"
             height="100%"
             frameBorder={0}
-            src="https://www.youtube.com/embed/H_ORu9ubzy8?si=h9kMIvKy1bR-i9Ri?mute=0&autoplay=1&controls=1&loop=1"
+            src="https://www.youtube.com/embed/H_ORu9ubzy8?si=h9kMIvKy1bR-i9Ri?mute=0&autoplay=0&controls=1&loop=1"
             title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allow="accelerometer;  clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             allowFullScreen></iframe>
         </div>
       </div>
--- components/UI/Footer.js
+++ components/UI/Footer.js
@@ -57,16 +57,16 @@ export default function Footer() {
         <div className={"max-w-xs font-matiz lg:text-lg xl:text-xl"}>
           <p>
             <span className="text-white/50">Email:</span>{" "}
-            <a href="mailto:info@gstone.co.nz">info@gstone.co.nz </a>
+            <a href="mailto:info@timelessfest.co.nz">info@timelessfest.co.nz </a>
           </p>
           <p>
-            <span className="text-white/50">Phone:</span> <a href="tel:073789542">07 378 9542 </a>
+            <span className="text-white/50">Phone:</span> <a href="">00 000 000 </a>
           </p>
         </div>
         <div className="w-px h-16 my-auto flex-shrink-0 bg-white/25 max-xl:hidden"></div>
         <div className={"max-w-xs font-matiz lg:text-lg xl:text-xl"}>
-          <p>PO Box 522,</p>
-          <p> Warkworth 0941 </p>
+          <p>PO Box </p>
+          <p> uknown </p>
         </div>
         <div className="w-px h-16 my-auto flex-shrink-0 bg-white/25 max-xl:hidden"></div>
       </div>
