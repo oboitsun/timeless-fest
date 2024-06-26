62b91614837ee158489e6404c40f2b7f6e40ba92
oboitsun <thewip3out@gmail.com>
2024-06-26 18:51:24 +0300

    updates + analytics

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import Script from "next/script";
 import Footer from "../components/UI/Footer";
 import Header from "../components/UI/Header";
@@ -36,6 +37,7 @@ export default function RootLayout({ children }) {
   ;(function(n,t,c){function i(n){return e._h?e._h.apply(null,n):e._q.push(n)}var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},once:function(){i(["once",c.call(arguments)])},off:function(){i(["off",c.call(arguments)])},get:function(){if(!e._h)throw new Error("[OpenWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},call:function(){i(["call",c.call(arguments)])},init:function(){var n=t.createElement("script");n.async=!0,n.type="text/javascript",n.src="https://cdn.openwidget.com/openwidget.js",t.head.appendChild(n)}};!n.__ow.asyncInit&&e.init(),n.OpenWidget=n.OpenWidget||e}(window,document,[].slice))`,
           }}
         />
+        <Analytics />
       </body>
     </html>
   );
