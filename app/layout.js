bc0804565006eb2ada2b6178b601d949d4c0ca96
oboitsun <thewip3out@gmail.com>
2024-06-27 08:39:11 +0300

    added meta

--- app/layout.js
+++ app/layout.js
@@ -14,6 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta name="facebook-domain-verification" content="xw9v1eeipdk4tl4ylcq2m6m2txmfqm" />
         <link href="https://fonts.cdnfonts.com/css/backcountry" rel="stylesheet" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={true.toString()} />
@@ -37,7 +38,7 @@ export default function RootLayout({ children }) {
   ;(function(n,t,c){function i(n){return e._h?e._h.apply(null,n):e._q.push(n)}var e={_q:[],_h:null,_v:"2.0",on:function(){i(["on",c.call(arguments)])},once:function(){i(["once",c.call(arguments)])},off:function(){i(["off",c.call(arguments)])},get:function(){if(!e._h)throw new Error("[OpenWidget] You can't use getters before load.");return i(["get",c.call(arguments)])},call:function(){i(["call",c.call(arguments)])},init:function(){var n=t.createElement("script");n.async=!0,n.type="text/javascript",n.src="https://cdn.openwidget.com/openwidget.js",t.head.appendChild(n)}};!n.__ow.asyncInit&&e.init(),n.OpenWidget=n.OpenWidget||e}(window,document,[].slice))`,
           }}
         />
-        <GoogleAnalytics gaId="G-Q451BFG64T" />
+        <GoogleAnalytics gaId="G-Q451BFG64T›" />
         <Analytics />
       </body>
     </html>
