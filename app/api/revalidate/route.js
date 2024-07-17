82528bfb514948b2be33ec3dc688df2157221486
oboitsun <thewip3out@gmail.com>
2024-07-17 13:17:47 +0300

    revalidate updates

--- app/api/revalidate/route.js
+++ app/api/revalidate/route.js
@@ -1,10 +1,12 @@
 import { revalidatePath } from "next/cache";
 
 export async function GET(req) {
+  revalidatePath("/nz", "page");
   revalidatePath("/nz/tickets", "page");
   revalidatePath("/nz/faq", "page");
   revalidatePath("/nz/news", "page");
   revalidatePath("/nz/set-times", "page");
+  revalidatePath("/aus", "page");
   revalidatePath("/aus/tickets", "page");
   revalidatePath("/aus/faq", "page");
   revalidatePath("/aus/news", "page");
