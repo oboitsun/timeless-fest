a3cf2b084d2bce682168f4a53d40b72674ef7e4d
oboitsun <thewip3out@gmail.com>
2024-07-25 16:35:44 +0300

    new updates

--- app/layout.js
+++ app/layout.js
@@ -38,7 +38,7 @@ fbq('init', '463061893331966');
 fbq('track', 'PageView');`,
           }}
         />
-        <GoogleAnalytics gaId="G-HPFD97CLFB›" />
+        <GoogleAnalytics gaId="G-Q451BFG64T›" />
         <Analytics />
       </body>
     </html>
